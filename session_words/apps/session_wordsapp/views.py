743407a1068d733d569fcfcb34bc017e69943fa3
Johnny Five <johnnyfive@johnny-five.local>
2017-09-19 22:23:16 -0500

    Session Words Project

---
+++ session_words/apps/session_wordsapp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
+
+
+# Create your views here.
+def index(request):
+    if 'activity' not in request.session:
+        request.session['activity'] = []
+        print "No Activity"
+    return render(request, 'session_wordsapp/index.html')
+
+def newword(request):
+    request.session['newword'] = request.POST['newword']
+    request.session['color'] = request.POST['color']
+    request.session['fontsize'] = request.POST.get('fontsize', "14px")
+
+    request.session['timestamp'] = strftime("%H:%M:%S %p, %B %d %Y", gmtime())
+
+    newword = {
+        "newword"   : request.session['newword'], 
+        "color"     : request.session['color'], 
+        "fontsize"  : request.session['fontsize'],
+        "timestamp" : request.session['timestamp']
+    }
+
+    request.session['activity'].append(newword)
+
+
+    print request.session['activity']
+    return redirect('/')
+
+def clearsession(request):
+    for key in request.session.keys():
+        del request.session[key]
+    print request.session
+    return redirect('/')
+
+
