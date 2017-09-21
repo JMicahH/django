9e7afa2aa5c0dda531d7ab247926b6dffa5e940a
Johnny Five <johnnyfive@johnny-five.local>
2017-09-21 11:04:43 -0500

    Amadon Project Completed

--- amadon/apps/shoppingcart/urls.py
+++ amadon/apps/shoppingcart/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from views import *
+
 urlpatterns = [
     url(r'^$', index, name = 'my_index'),
     url(r'^purchase$', purchase ),
-    url(r'^process$', process)
+    url(r'^checkout$', checkout),
+    url(r'^reset', reset)
 ]
--- amadon/apps/shoppingcart/views.py
+++ amadon/apps/shoppingcart/views.py
@@ -1,35 +1,48 @@
 from django.shortcuts import render, HttpResponse, redirect
 
-# asdfCreate your views here.
+# Create your views here.
 def index(request):
-
-    return render(request,'shoppingcart/index.html')
+    if 'count' not in request.session:
+        request.session['count'] = 0
+        request.session['grandtotal'] = 0
+    return render(request, 'shoppingcart/index.html')
 
 def purchase(request):
     products = {
         '1001' : {
             'name'  : 'Dojo T-Shirt',
-            'price' : '19.99',
-        } 
+            'price' : '1999'
+        }, 
         '1002' : {
             'name'  : 'Dojo Sweater',
-            'price' : '29.99',
-        } 
+            'price' : '2999'
+        }, 
         '1003' : {
             'name'  : 'Dojo cup',
-            'price' : '4.99',
-        } 
+            'price' : '499'
+        }, 
         '1004' : {
             'name'  : 'Algorithm Book',
-            'price' : '49.99',
-        } 
+            'price' : '4999'
+        }, 
     }
     
     product_id = request.POST['product_id']
-    print products[product_id]
+    quantity = int(request.POST['quantity'])
+    product_price = int(products[product_id]['price'])
 
-    return redirect('/process')
 
-def process(request):
+    request.session['total'] = float(product_price * quantity)/100
+    request.session['count'] += quantity
+    request.session['grandtotal'] += request.session['total']
+    return redirect('/checkout')
+
+def checkout(request):
 
     return render(request, 'shoppingcart/checkout.html')
+
+def reset(request):
+    for key in request.session.keys():
+        del request.session[key]
+    print request.session
+    return redirect('/')
